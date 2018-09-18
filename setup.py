df87334f3fd99253fc9e28df54b0be146b6c1a0b
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-09-18 09:21:33 +0200

    Added SSL for elastic connection

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=['sokannonser'],
     include_package_data=True,
     install_requires=[
-        'flask', 'flask-restplus', 'flask-cors', 'elasticsearch'
+        'flask', 'flask-restplus', 'flask-cors', 'elasticsearch', 'certifi'
     ],
 )
--- sokannonser/repository/__init__.py
+++ sokannonser/repository/__init__.py
@@ -7,6 +7,7 @@ log = logging.getLogger(__name__)
 log.info("Using Elasticsearch node at %s:%s" % (settings.ES_HOST, settings.ES_PORT))
 if settings.ES_USER and settings.ES_PWD:
     elastic = Elasticsearch([{'host': settings.ES_HOST, 'port': settings.ES_PORT,
+                              'use_ssl': True,
                               'http_auth': "%s:%s" % (settings.ES_USER,
                                                       settings.ES_PWD)}])
 else:

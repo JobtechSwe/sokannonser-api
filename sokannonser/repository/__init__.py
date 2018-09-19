82c44ca87c6049c59700a4a17c6cf1cd6284c444
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-09-19 09:18:19 +0200

    Use ssl_context

--- sokannonser/repository/__init__.py
+++ sokannonser/repository/__init__.py
@@ -1,4 +1,6 @@
 import logging
+import certifi
+from ssl import create_default_context
 from elasticsearch import Elasticsearch
 from sokannonser import settings
 
@@ -6,9 +8,11 @@ log = logging.getLogger(__name__)
 
 log.info("Using Elasticsearch node at %s:%s" % (settings.ES_HOST, settings.ES_PORT))
 if settings.ES_USER and settings.ES_PWD:
+    context = create_default_context(cafile=certifi.where())
     elastic = Elasticsearch([settings.ES_HOST], port=settings.ES_PORT,
                             use_ssl=True, scheme='https', sniff_on_start=False,
                             sniff_on_connection_fail=True, sniffer_timeout=60,
+                            ssl_context=context,
                             http_auth=(settings.ES_USER, settings.ES_PWD))
 else:
     elastic = Elasticsearch([{'host': settings.ES_HOST, 'port': settings.ES_PORT}])

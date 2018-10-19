120a275e9e80e3cd3f5d5dc3fbaae67bafc0c3cc
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-10-19 14:32:05 +0200

    Skeletal typeahead for auranest

--- sokannonser/rest/__init__.py
+++ sokannonser/rest/__init__.py
@@ -5,10 +5,12 @@ api = Api(version='1.0', title='Sök Annonser',
           default='sokannonser',
           default_label="Verktyg för att hitta platsannoner")
 
-ns_afannons = Namespace('AF-Annonser', description='Sök bland AF:s annonser')
+ns_afannons = Namespace('AF-Annonser',
+                        description='Sök bland AF:s annonser')
 ns_auranest = Namespace('Alla Annonser',
                         description='Sök bland alla annonser på marknaden')
-ns_valuestore = Namespace('Värdeförråd', description='Sök i taxonomi och ontologi')
+ns_valuestore = Namespace('Värdeförråd',
+                          description='Sök i taxonomi och ontologi')
 
 api.add_namespace(ns_afannons, '/af')
 api.add_namespace(ns_auranest, '/alla')

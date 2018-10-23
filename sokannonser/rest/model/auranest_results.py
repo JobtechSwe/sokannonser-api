0c7a0df93adaf53151172618f8018e92e54abb9d
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-10-23 16:54:52 +0200

    Nested models as objects

--- sokannonser/rest/model/auranest_results.py
+++ sokannonser/rest/model/auranest_results.py
@@ -1,27 +1,33 @@
 from flask_restplus import fields
 from sokannonser.rest import ns_auranest
 
+
+employer = ns_auranest.model('Employer', {
+    'name': fields.String(),
+    'logoUrl': fields.String()
+})
+
+site = ns_auranest.model('Site', {
+    'url': fields.String(),
+    'name': fields.String()
+})
+
+application = ns_auranest.model('Application', {
+    'url': fields.String(),
+    'email': fields.String(),
+    'deadline': fields.DateTime(),
+    'reference': fields.String(),
+    'site': fields.Nested(site, attribute='site')
+})
+
 annons = ns_auranest.model('Annons', {
     'id': fields.String(attribute='_source.id'),
     'header': fields.String(attribute='_source.header'),
     'content': fields.String(attribute='_source.content.text'),
-    'employer': fields.Nested({
-        'name': fields.String(),
-        'logoUrl': fields.String()
-    }, attribute='employer', skip_none=True),
+    'employer': fields.Nested(employer, attribute='employer', skip_none=True),
     'location': fields.String(attribute='_source.location.translations.sv-SE'),
-    'application': fields.Nested({
-        'url': fields.String(),
-        'email': fields.String(),
-        'deadline': fields.DateTime(),
-        'reference': fields.String(),
-        'site': fields.Nested({
-            'url': fields.String(),
-            'name': fields.String()
-        })
-    }, attribute='_source.application', skip_none=True)
+    'application': fields.Nested(application, attribute='_source.application', skip_none=True)
 })
-
 
 stat_item = ns_auranest.model('StatItem', {
     'value': fields.String(attribute='key'),

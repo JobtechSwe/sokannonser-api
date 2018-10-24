8ab845c951cda6da892cbbcfa01a527f9fcd0427
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-10-24 11:02:48 +0200

    No need to show result model in swagger

--- sokannonser/rest/model/auranest_results.py
+++ sokannonser/rest/model/auranest_results.py
@@ -1,48 +1,40 @@
 from flask_restplus import fields
-from sokannonser.rest import ns_auranest
+from sokannonser.rest import ns_auranest as api
 
 
-employer = ns_auranest.model('Employer', {
-    'name': fields.String(),
-    'logoUrl': fields.String()
-})
-
-site = ns_auranest.model('Site', {
-    'url': fields.String(),
-    'name': fields.String()
-})
-
-application = ns_auranest.model('Application', {
-    'url': fields.String(),
-    'email': fields.String(),
-    'deadline': fields.DateTime(),
-    'reference': fields.String(),
-    'site': fields.Nested(site, attribute='site')
-})
-
-annons = ns_auranest.model('Annons', {
+annons = api.model('Ad', {
     'id': fields.String(attribute='_source.id'),
     'header': fields.String(attribute='_source.header'),
     'content': fields.String(attribute='_source.content.text'),
-    'employer': fields.Nested(employer, attribute='employer', skip_none=True),
+    'employer': fields.Nested({
+        'name': fields.String(),
+        'logoUrl': fields.String()
+    }, attribute='employer', skip_none=True),
     'location': fields.String(attribute='_source.location.translations.sv-SE'),
-    'application': fields.Nested(application, attribute='_source.application', skip_none=True)
+    'application': fields.Nested({
+        'url': fields.String(),
+        'email': fields.String(),
+        'deadline': fields.DateTime(),
+        'reference': fields.String(),
+        'site': fields.Nested({
+            'url': fields.String(),
+            'name': fields.String()
+        }, attribute='site')
+    }, attribute='_source.application', skip_none=True)
 })
 
-stat_item = ns_auranest.model('StatItem', {
+stat_value = api.model('StatValue', {
     'value': fields.String(attribute='key'),
     'count': fields.Integer(attribute='doc_count')
 })
 
-statistics = ns_auranest.model('Statistics', {
-    'employers': fields.List(fields.Nested(stat_item), attribute='employers.buckets'),
-    'sites': fields.List(fields.Nested(stat_item), attribute='sites.buckets'),
-    'locations': fields.List(fields.Nested(stat_item), attribute='locations.buckets'),
-})
-
-auranest_lista = ns_auranest.model('Annonser', {
+auranest_lista = api.model('Ads', {
     'total': fields.Integer(attribute='hits.total'),
-    'stats': fields.Nested(statistics, attribute='aggregations', skip_none=True),
+    'stats': fields.Nested({
+        'employers': fields.List(fields.Nested(stat_value), attribute='employers.buckets'),
+        'sites': fields.List(fields.Nested(stat_value), attribute='sites.buckets'),
+        'locations': fields.List(fields.Nested(stat_value), attribute='locations.buckets'),
+    }, attribute='aggregations', skip_none=True),
     'hits': fields.List(fields.Nested(annons), attribute='hits.hits')
 })
 

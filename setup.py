6c6e6297d1fecc8bc2154ce429cc08b65dd37b73
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-11-20 15:17:29 +0100

    Adds requests as requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     packages=['sokannonser'],
     include_package_data=True,
     install_requires=[
-        'valuestore', 'flask', 'flask-restplus', 'flask-cors', 'elasticsearch', 'certifi'
+        'valuestore', 'flask', 'flask-restplus', 'flask-cors', 'elasticsearch', 'certifi',
+        'requests'
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"]

2008bf6386d7247bc816e72abbc6711320d03f7c
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-11-22 13:58:05 +0100

    Separates typeahead to it's own endpoint

--- sokannonser/rest/endpoint/platsannonser.py
+++ sokannonser/rest/endpoint/platsannonser.py
@@ -5,7 +5,8 @@ from sokannonser import settings
 from sokannonser.rest import ns_platsannons, ns_open
 from sokannonser.rest.decorators import check_api_key
 from sokannonser.rest.model.platsannons_results import simple_lista
-from sokannonser.rest.model.queries import sok_platsannons_query, swagger_doc_params
+from sokannonser.rest.model.queries import sok_platsannons_query, pb_query
+from sokannonser.rest.model.queries import swagger_doc_params, swagger_filter_doc_params
 from sokannonser.repository import platsannonser
 from sokannonser.repository.querybuilder import QueryBuilder
 
@@ -14,6 +15,37 @@ log = logging.getLogger(__name__)
 
 @ns_platsannons.route('/search')
 class PBSearch(Resource):
+    method_decorators = [check_api_key]
+    querybuilder = QueryBuilder()
+
+    @ns_platsannons.doc(
+        params={**swagger_doc_params, **swagger_filter_doc_params},
+        responses={
+            200: 'OK',
+            401: 'Felaktig API-nyckel',
+            500: 'Bad'
+        }
+    )
+    @ns_platsannons.expect(pb_query)
+    def get(self):
+        args = pb_query.parse_args()
+        result = platsannonser.find_platsannonser(args, self.querybuilder)
+
+        return self.marshal_results(result)
+
+    def marshal_results(self, esresult):
+        result = {
+            "total": esresult.get('total', 0),
+            "positions": esresult.get('positions', 0),
+            "typeahead": esresult.get('aggs', []),
+            "stats": esresult.get('stats', {}),
+            "hits": [hit['_source'] for hit in esresult['hits']],
+        }
+        return result
+
+
+@ns_platsannons.route('/complete')
+class PBComplete(Resource):
     method_decorators = [check_api_key]
     querybuilder = QueryBuilder()
 
@@ -28,6 +60,9 @@ class PBSearch(Resource):
     @ns_platsannons.expect(sok_platsannons_query)
     def get(self):
         args = sok_platsannons_query.parse_args()
+        args[settings.LIMIT] = 0  # Always return 0 ads when calling typeahead
+        args[settings.TYPEAHEAD_QUERY] = args.get(settings.FREETEXT_QUERY)
+
         result = platsannonser.find_platsannonser(args, self.querybuilder)
 
         return self.marshal_results(result)
@@ -37,8 +72,6 @@ class PBSearch(Resource):
             "total": esresult.get('total', 0),
             "positions": esresult.get('positions', 0),
             "typeahead": esresult.get('aggs', []),
-            "stats": esresult.get('stats', {}),
-            "hits": [hit['_source'] for hit in esresult['hits']],
         }
         return result
 

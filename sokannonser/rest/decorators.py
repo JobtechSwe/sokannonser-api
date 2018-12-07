67918def19585192b8def4fbd5ee978aa21f6f3e
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-12-07 10:58:20 +0100

    Checks API keys against index every 60 seconds

--- sokannonser/rest/decorators.py
+++ sokannonser/rest/decorators.py
@@ -2,14 +2,20 @@ import logging
 import base64
 import binascii
 import re
+import time
 from flask import request
 from flask_restplus import abort
 from sokannonser import settings
+from sokannonser.repository import elastic
 
 
 log = logging.getLogger(__name__)
 
 EMAIL_REGEX = re.compile(r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?")
+valid_api_keys = elastic.get_source(index=settings.ES_SYSTEM_INDEX,
+                                    doc_type='_all',
+                                    id=settings.ES_APIKEYS_DOC_ID)
+last_check_ts = int(time.time())
 
 
 def check_api_key(func):
@@ -28,12 +34,18 @@ def check_api_key(func):
 
 def check_api_key_auranest(func):
     def wrapper(*args, **kwargs):
+        global last_check_ts, valid_api_keys
+        if int(time.time()) > last_check_ts + 60:  # Refresh keys every 60 secs
+            log.debug("Reloading API keys")
+            valid_api_keys = elastic.get_source(index=settings.ES_SYSTEM_INDEX,
+                                                doc_type='_all',
+                                                id=settings.ES_APIKEYS_DOC_ID)
+            last_check_ts = time.time()
         apikey = request.headers.get(settings.APIKEY)
-        decoded_key = _decode_key(apikey)
-        if EMAIL_REGEX.match(decoded_key):
-            log.info("API key %s is valid." % decoded_key)
+        if valid_api_keys and apikey in valid_api_keys.get('validkeys', []):
+            log.info("API key \"%s\" is valid." % _decode_key(apikey))
             return func(*args, **kwargs)
-        log.info("Failed validation for key '%s'" % decoded_key)
+        log.info("Failed validation for key '%s'" % apikey)
         abort(401, message="You're no monkey!")
 
     return wrapper
@@ -41,14 +53,15 @@ def check_api_key_auranest(func):
 
 # Decodes the API which is in base64 format
 def _decode_key(apikey):
-    decoded = 'Invalid API key'
+    decoded = apikey
     if apikey:
         for i in range(3):
             try:
                 decoded = base64.urlsafe_b64decode(apikey).decode('utf-8').strip()
                 break
             except binascii.Error as e:
-                log.debug("Failed to decode api key: %s: %s" % (apikey, e))
+                # log.debug("Failed to decode api key: %s: %s" % (apikey, e))
+                pass
             except UnicodeDecodeError as u:
                 log.debug("Failed to decode utf-8 key: %s: %s" % (apikey, u))
             # Reappend trailing '=' to find correct padding

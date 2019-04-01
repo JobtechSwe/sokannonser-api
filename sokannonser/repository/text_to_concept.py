efe8fd9ec64dbd6209ef06f99081373451232463
penms <mattias.persson@arbetsformedlingen.se>
2019-04-01 11:27:51 +0200

    Fixed error with query rewrite when using plus sign for known ontology terms.

--- sokannonser/repository/text_to_concept.py
+++ sokannonser/repository/text_to_concept.py
@@ -2,10 +2,15 @@ import logging
 import certifi
 from ssl import create_default_context
 from elasticsearch import Elasticsearch
+from copy import copy
 from sokannonser import settings
 from sokannonser.repository.ontology import Ontology
 
 log = logging.getLogger(__name__)
+
+OP_NONE = ''
+OP_PLUS = '+'
+OP_MINUS = '-'
 
 
 class TextToConcept(object):
@@ -26,7 +31,6 @@ class TextToConcept(object):
         if settings.ES_HOST != 'localhost':
             # Cache ontology directly unless it's a local call (tests or docker build)
             self.get_ontology()
-
 
     def get_ontology(self):
         if self.ontology is None:
@@ -55,39 +59,56 @@ class TextToConcept(object):
         return client
 
     def text_to_concepts(self, text):
-        ontology_concepts = self.get_ontology().get_concepts(text, concept_type=None,
-                                                             span_info=False)
+        ontology_concepts_orig = self.get_ontology().get_concepts(text, concept_type=None,
+                                                                  span_info=True)
+
+        ontology_concepts = []
+        for concept_data in ontology_concepts_orig:
+            concept = copy(concept_data[0])
+            concept['span_begin'] = concept_data[1]
+            concept['span_end'] = concept_data[2]
+            ontology_concepts.append(concept)
+
+        # print(ontology_concepts)
         text_lower = text.lower()
 
-        tmp_text = text_lower
-
         for concept in ontology_concepts:
-            term = concept['term']
-            term_index = tmp_text.index(term) - 1
-            prev_char = tmp_text[term_index:term_index + 1]
-            # print('term: %s, prev_char: %s' % (term, prev_char))
-            tmp_text = tmp_text.replace(term, self.REMOVED_TAG)
-            concept['operator'] = prev_char if prev_char == '-' else ''
-
-        # print(tmp_text)
+            term_index = concept['span_begin']
+            prev_char = text_lower[term_index - 1:term_index]
+            if prev_char == OP_MINUS:
+                concept['operator'] = OP_MINUS
+            elif prev_char == OP_PLUS:
+                concept['operator'] = OP_PLUS
+            else:
+                concept['operator'] = OP_NONE
 
         skills = [c['concept'].lower() for c in ontology_concepts
-                  if self.filter_concepts(c, self.COMPETENCE_KEY, '')]
+                  if self.filter_concepts(c, self.COMPETENCE_KEY, OP_NONE)]
         occupations = [c['concept'].lower() for c in ontology_concepts
-                       if self.filter_concepts(c, self.OCCUPATION_KEY, '')]
+                       if self.filter_concepts(c, self.OCCUPATION_KEY, OP_NONE)]
         traits = [c['concept'].lower() for c in ontology_concepts
-                  if self.filter_concepts(c, self.TRAIT_KEY, '')]
+                  if self.filter_concepts(c, self.TRAIT_KEY, OP_NONE)]
+
+        skills_must = [c['concept'].lower() for c in ontology_concepts
+                       if self.filter_concepts(c, self.COMPETENCE_KEY, OP_PLUS)]
+        occupations_must = [c['concept'].lower() for c in ontology_concepts
+                            if self.filter_concepts(c, self.OCCUPATION_KEY, OP_PLUS)]
+        traits_must = [c['concept'].lower() for c in ontology_concepts
+                       if self.filter_concepts(c, self.TRAIT_KEY, OP_PLUS)]
 
         skills_must_not = [c['concept'].lower() for c in ontology_concepts
-                           if self.filter_concepts(c, self.COMPETENCE_KEY, '-')]
+                           if self.filter_concepts(c, self.COMPETENCE_KEY, OP_MINUS)]
         occupations_must_not = [c['concept'].lower() for c in ontology_concepts
-                                if self.filter_concepts(c, self.OCCUPATION_KEY, '-')]
+                                if self.filter_concepts(c, self.OCCUPATION_KEY, OP_MINUS)]
         traits_must_not = [c['concept'].lower() for c in ontology_concepts
-                           if self.filter_concepts(c, self.TRAIT_KEY, '-')]
+                           if self.filter_concepts(c, self.TRAIT_KEY, OP_MINUS)]
 
         result = {'skills': skills,
                   'occupations': occupations,
                   'traits': traits,
+                  'skills_must': skills_must,
+                  'occupations_must': occupations_must,
+                  'traits_must': traits_must,
                   'skills_must_not': skills_must_not,
                   'occupations_must_not': occupations_must_not,
                   'traits_must_not': traits_must_not}

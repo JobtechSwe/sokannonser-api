c0dfcb22553852491d59f2a13dd95c9668918d56
Markus Sköld <markus.skold@arbetsformedlingen.se>
2019-04-02 14:58:27 +0200

    Ads streaming download for bulkloader

--- bulkloader/rest/__init__.py
+++ bulkloader/rest/__init__.py
@@ -11,8 +11,14 @@ ns_bulk = Namespace('Bulk loader', description='Endpoint for downloading all ads
 
 api.add_namespace(ns_bulk, '/bulk')
 
-bulk_query = reqparse.RequestParser()
-bulk_query.add_argument(settings.APIKEY, location='headers', required=True)
-bulk_query.add_argument(settings.DATE,
-                        type=inputs.regex('^\\d{4}-\\d{2}-\\d{2}|all|yesterday$'),
-                        required=True)
+bulk_zip_query = reqparse.RequestParser()
+bulk_zip_query.add_argument(settings.APIKEY, location='headers', required=True)
+bulk_zip_query.add_argument(settings.DATE,
+                            type=inputs.regex('^\\d{4}-\\d{2}-\\d{2}|all|yesterday$'),
+                            required=True)
+
+bulk_stream_query = reqparse.RequestParser()
+bulk_stream_query.add_argument(settings.APIKEY, location='headers', required=True)
+bulk_stream_query.add_argument(settings.DATE,
+                               type=inputs.datetime_from_iso8601,
+                               required=True)

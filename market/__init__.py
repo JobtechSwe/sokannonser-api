bfa6464b192bfff4b08a7c34607e28c33626c0f8
Markus Sköld <markus.skold@arbetsformedlingen.se>
2019-04-03 13:52:08 +0200

    Adds broken out code

---
+++ market/__init__.py
@@ -0,0 +1,24 @@
+import logging
+from flask import Flask
+from flask_cors import CORS
+from sokannonser import appconf
+from market.rest import api
+# Import all Resources that are to be made visible for the app
+from market.rest.endpoint import MarketSearch
+from market.rest.endpoint import MarketComplete
+
+app = Flask(__name__)
+CORS(app)
+appconf.configure_logging()
+log = logging.getLogger(__name__)
+log.debug(logging.getLevelName(log.getEffectiveLevel()) + ' log level activated')
+log.info("Starting %s" % __name__)
+
+
+if __name__ == '__main__':
+    # Used only when starting this script directly, i.e. for debugging
+    appconf.initialize_app(app, api)
+    app.run(debug=True)
+else:
+    # Main entrypoint
+    appconf.initialize_app(app, api)
---
+++ market/rest/endpoint.py
@@ -0,0 +1,32 @@
+from flask_restplus import Resource
+from jobtech.common.rest.decorators import check_api_key
+from sokannonser import settings
+from market.rest import ns_market, market_query, market_typeahead
+from market.rest.results import market_list
+from market import repository
+
+
+@ns_market.route('/search')
+class MarketSearch(Resource):
+    method_decorators = [check_api_key('all')]
+
+    @ns_market.doc(description='Search with freetext query')
+    @ns_market.expect(market_query)
+    def get(self):
+        args = market_query.parse_args()
+        return self.marshal_default(repository.find_annonser(args))
+
+    @ns_market.marshal_with(market_list)
+    def marshal_default(self, results):
+        return results
+
+
+@ns_market.route('/complete')
+class MarketComplete(Resource):
+    method_decorators = [check_api_key('all')]
+
+    @ns_market.doc(description='Typeahead / Suggest the next search term')
+    @ns_market.expect(market_typeahead)
+    def get(self):
+        args = market_typeahead.parse_args()
+        return repository.autocomplete(args.get(settings.FREETEXT_QUERY))

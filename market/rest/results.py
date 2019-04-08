a78f73a34593bc09902622744cbcd43e01430014
Markus Sköld <markus.skold@arbetsformedlingen.se>
2019-04-08 13:09:39 +0200

    Ads model for market search endpoint

--- market/rest/results.py
+++ market/rest/results.py
@@ -27,36 +27,47 @@ class ShortString(fields.Raw):
         return new_value
 
 
+employer = api.model('Employer', {
+    'name': fields.String(),
+    'logoUrl': fields.String()
+})
+
+site = api.model('Site', {
+    'url': fields.String(),
+    'name': fields.String()
+})
+
+application = api.model('Application', {
+    'url': fields.String(),
+    'email': fields.String(),
+    'deadline': fields.DateTime(),
+    'reference': fields.String(),
+    'site': fields.Nested(site, attribute='site')
+})
+
+keywords = api.model('DetectedKeywords', {
+    'occupations': fields.List(fields.String),
+    'skills': fields.List(fields.String),
+    'traits': fields.List(fields.String)
+})
+
+sources = api.model('Sources', {
+    'id': fields.String(attribute='_source.id'),
+    'name': fields.String(attribute='_source.source.site.name'),
+    'url': fields.String(attribute='_source.source.url'),
+})
+
 annons = api.model('Ad', {
     'id': fields.String(attribute='_source.id'),
     'header': fields.String(attribute='_source.header'),
     'content': ShortString(attribute='_source.content.text'),
     'markup': ShortString(attribute='_source.content.xml'),
-    'employer': fields.Nested({
-        'name': fields.String(),
-        'logoUrl': fields.String()
-    }, attribute='_source.employer', skip_none=True),
+    'employer': fields.Nested(employer, attribute='_source.employer', skip_none=True),
     'location': fields.String(attribute='_source.location.translations.sv-SE'),
-    'application': fields.Nested({
-        'url': fields.String(),
-        'email': fields.String(),
-        'deadline': fields.DateTime(),
-        'reference': fields.String(),
-        'site': fields.Nested({
-            'url': fields.String(),
-            'name': fields.String()
-        }, attribute='site')
-    }, attribute='_source.application', skip_none=True),
-    'detected_keywords': fields.Nested({
-        'occupations': fields.List(fields.String),
-        'skills': fields.List(fields.String),
-        'traits': fields.List(fields.String)
-    }, attribute='_source'),
-    'sources': fields.List(fields.Nested({
-        'id': fields.String(attribute='_source.id'),
-        'name': fields.String(attribute='_source.source.site.name'),
-        'url': fields.String(attribute='_source.source.url'),
-    }), attribute='inner_hits.other.hits.hits')
+    'application': fields.Nested(application, attribute='_source.application',
+                                 skip_none=True),
+    'detected_keywords': fields.Nested(keywords, attribute='_source'),
+    'sources': fields.List(fields.Nested(sources), attribute='inner_hits.other.hits.hits')
 })
 
 stat_value = api.model('StatValue', {
@@ -64,15 +75,17 @@ stat_value = api.model('StatValue', {
     'count': fields.Integer(attribute='doc_count')
 })
 
+stats = api.model('Stats', {
+    'employers': fields.List(fields.Nested(stat_value),
+                             attribute='employers.buckets'),
+    'sites': fields.List(fields.Nested(stat_value),
+                         attribute='sites.buckets'),
+    'locations': fields.List(fields.Nested(stat_value),
+                             attribute='locations.buckets'),
+})
+
 market_list = api.model('Ads', {
     'total': fields.Integer(attribute='aggregations.total.value'),
-    'stats': fields.Nested({
-        'employers': fields.List(fields.Nested(stat_value),
-                                 attribute='employers.buckets'),
-        'sites': fields.List(fields.Nested(stat_value),
-                             attribute='sites.buckets'),
-        'locations': fields.List(fields.Nested(stat_value),
-                                 attribute='locations.buckets'),
-    }, attribute='aggregations', skip_none=True),
+    'stats': fields.Nested(stats, attribute='aggregations', skip_none=True),
     'hits': fields.List(fields.Nested(annons), attribute='hits.hits')
 })

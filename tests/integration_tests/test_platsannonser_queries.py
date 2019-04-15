d4382fe3a5e6c98f35dfad4aae18a96ca4b9d100
penms <mattias.persson@arbetsformedlingen.se>
2019-04-15 10:45:58 +0200

    Removed ['keywords']['enriched'] from output since it should only be used for searching and not to be returned to end user.

--- tests/integration_tests/test_platsannonser_queries.py
+++ tests/integration_tests/test_platsannonser_queries.py
@@ -6,7 +6,7 @@ from sokannonser import app
 
 test_api_key = os.getenv('TEST_API_KEY_PLATSANNONSER')
 
-# @pytest.mark.skip(reason="Temporarily disabled")
+@pytest.mark.skip(reason="Temporarily disabled")
 @pytest.mark.integration
 def test_freetext_query_one_param():
     print('============================', sys._getframe().f_code.co_name, '============================ ')
@@ -14,13 +14,34 @@ def test_freetext_query_one_param():
     app.testing = True
     with app.test_client() as testclient:
         headers = {'api-key': test_api_key, 'accept': 'application/json'}
-        result = testclient.get('/af/search', headers=headers, data={'q': 'gymnasielärare', 'limit': '0'})
+        result = testclient.get('/open/search', headers=headers, data={'q': 'gymnasielärare', 'limit': '1'})
         json_response = result.json
         # pprint(json_response)
         hits_total = json_response['total']
         assert int(hits_total) > 0
 
-# @pytest.mark.skip(reason="Temporarily disabled")
+@pytest.mark.skip(reason="Temporarily disabled")
+@pytest.mark.integration
+def test_freetext_query_one_param_deleted_enriched():
+    print('============================', sys._getframe().f_code.co_name, '============================ ')
+
+    app.testing = True
+    with app.test_client() as testclient:
+        headers = {'api-key': test_api_key, 'accept': 'application/json'}
+        result = testclient.get('/open/search', headers=headers, data={'q': 'gymnasielärare', 'limit': '10'})
+        json_response = result.json
+        # pprint(json_response)
+        hits_total = json_response['total']
+        assert int(hits_total) > 0
+        hits = json_response['hits']
+        assert len(hits) > 0
+        # pprint(hits[0])
+
+        assert 'extracted' in hits[0]['keywords']
+        assert 'enriched' not in hits[0]['keywords']
+
+
+@pytest.mark.skip(reason="Temporarily disabled")
 @pytest.mark.integration
 def test_freetext_query_two_params():
     print('============================', sys._getframe().f_code.co_name, '============================ ')
@@ -28,7 +49,7 @@ def test_freetext_query_two_params():
     app.testing = True
     with app.test_client() as testclient:
         headers = {'api-key': test_api_key, 'accept': 'application/json'}
-        result = testclient.get('/af/search', headers=headers, data={'q': 'gymnasielärare lokförare', 'limit': '0'})
+        result = testclient.get('/open/search', headers=headers, data={'q': 'gymnasielärare lokförare', 'limit': '0'})
         json_response = result.json
         # pprint(json_response)
         hits_total = json_response['total']

7fd25d3f704fdf6c357c1754668e05536bdd8adc
penms <mattias.persson@arbetsformedlingen.se>
2019-04-24 13:25:03 +0200

    Added flag found_in_enriched in result, True if any of the freetext input words corresponds to any enriched term.

--- tests/integration_tests/test_platsannonser_queries.py
+++ tests/integration_tests/test_platsannonser_queries.py
@@ -43,6 +43,48 @@ def test_freetext_query_one_param_deleted_enriched():
 
 @pytest.mark.skip(reason="Temporarily disabled")
 @pytest.mark.integration
+def test_freetext_query_one_param_found_in_enriched_pos():
+    print('============================', sys._getframe().f_code.co_name, '============================ ')
+
+    app.testing = True
+    with app.test_client() as testclient:
+        headers = {'api-key': test_api_key, 'accept': 'application/json'}
+        result = testclient.get('/open/search', headers=headers, data={'q': 'diskare', 'limit': '100'})
+        json_response = result.json
+        pprint(json_response)
+        # hits_total = json_response['total']
+        # assert int(hits_total) > 0
+        hits = json_response['hits']
+
+        # assert len(hits) > 0
+        pprint(hits[0])
+        assert 'found_in_enriched' in hits[0]
+
+
+@pytest.mark.skip(reason="Temporarily disabled")
+@pytest.mark.integration
+def test_freetext_query_one_param_found_in_enriched_neg():
+    print('============================', sys._getframe().f_code.co_name, '============================ ')
+
+    app.testing = True
+    with app.test_client() as testclient:
+        headers = {'api-key': test_api_key, 'accept': 'application/json'}
+        result = testclient.get('/open/search', headers=headers, data={'q': 'ninja', 'limit': '100'})
+        json_response = result.json
+        pprint(json_response)
+        # hits_total = json_response['total']
+        # assert int(hits_total) > 0
+        hits = json_response['hits']
+        # assert len(hits) > 0
+        # pprint(hits[0])
+        for hit in hits:
+            assert 'found_in_enriched' in hit
+            assert hit['found_in_enriched'] is False
+
+
+
+@pytest.mark.skip(reason="Temporarily disabled")
+@pytest.mark.integration
 def test_freetext_query_two_params():
     print('============================', sys._getframe().f_code.co_name, '============================ ')
 

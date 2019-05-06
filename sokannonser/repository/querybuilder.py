9d78b3ff6541571c406fe43e75d3a3f3d038c724
Markus Sköld <markus.skold@arbetsformedlingen.se>
2019-05-06 14:10:49 +0200

    Log typeahead in the same way as query

--- sokannonser/repository/querybuilder.py
+++ sokannonser/repository/querybuilder.py
@@ -128,7 +128,9 @@ class QueryBuilder(object):
         query_dsl = dict()
         query_dsl['from'] = args.pop(settings.OFFSET, 0)
         query_dsl['size'] = args.pop(settings.LIMIT, 10)
-        query_dsl['track_total_hits'] = True
+        # No need to track all results if used for typeahead
+        if not args.get(settings.TYPEAHEAD_QUERY):
+            query_dsl['track_total_hits'] = True
         if args.pop(settings.DETAILS, '') == queries.OPTIONS_BRIEF:
             query_dsl['_source'] = [f.ID, f.HEADLINE, f.APPLICATION_DEADLINE,
                                     f.EMPLOYMENT_TYPE+"."+f.LABEL,

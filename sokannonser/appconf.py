f43f192ac7125bd6d07621c72211c20498e9036c
Markus Sköld <markus.skold@arbetsformedlingen.se>
2019-05-13 14:11:43 +0200

    Moves log configuration into jobtech common

--- sokannonser/appconf.py
+++ sokannonser/appconf.py
@@ -8,56 +8,6 @@ from elasticapm.contrib.flask import ElasticAPM
 log = logging.getLogger(__name__)
 
 
-def configure_logging():
-    logging.basicConfig()
-    # Remove basicConfig-handlers and replace with custom formatter.
-    for handler in logging.root.handlers[:]:
-        logging.root.removeHandler(handler)
-    stream_handler = logging.StreamHandler()
-
-    f = create_log_formatter()
-    stream_handler.setFormatter(f)
-    root = logging.getLogger()
-    # root.setLevel(logging.INFO)
-    root.addHandler(stream_handler)
-
-    set_custom_log_level()
-
-
-def create_log_formatter():
-    if os.getenv('FLASK_ENV', '') == 'development':
-        is_develop_mode = True
-    else:
-        is_develop_mode = False
-    f = JobtechLogFormatter('%(asctime)s|%(levelname)s|%(name)s|MESSAGE: %(message)s',
-                            '%Y-%m-%d %H:%M:%S%z', is_develop_mode=is_develop_mode)
-    return f
-
-
-def set_custom_log_level():
-    # Set log level debug for module specific events
-    # and level warning for all third party dependencies
-    master_module = str(__name__).split('.')[0]
-    for key in logging.Logger.manager.loggerDict:
-        # for handler in logging.getLogger(key).handlers[:]:
-        #     logging.getLogger(key).removeHandler(handler)
-        if key.startswith(master_module) or key.startswith('market') \
-                or key.startswith('bulkloader') or key.startswith('jobtech'):
-            if os.getenv('LOGLEVEL', '') == 'debug':
-                print("Setting loglevel DEBUG for %s" % key)
-                logging.getLogger(key).setLevel(logging.DEBUG)
-            else:
-                print("Setting loglevel INFO for %s" % key)
-                logging.getLogger(key).setLevel(logging.INFO)
-
-
-        else:
-            logging.getLogger(key).setLevel(logging.WARNING)
-
-
-# NarvalLogFormatter.printTestLogMessages(log)
-
-
 def configure_app(flask_app):
     flask_app.config.SWAGGER_UI_DOC_EXPANSION = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
     flask_app.config.RESTPLUS_VALIDATE = settings.RESTPLUS_VALIDATE

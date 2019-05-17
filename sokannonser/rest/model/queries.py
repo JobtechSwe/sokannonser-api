59ecca6451c0f9f15268f802a9ad2e08385902db
Markus Sköld <markus.skold@arbetsformedlingen.se>
2019-05-17 13:51:02 +0200

    Adds sorting by ID

--- sokannonser/rest/model/queries.py
+++ sokannonser/rest/model/queries.py
@@ -16,61 +16,63 @@ OPTIONS_FULL = 'full'
 swagger_doc_params = {
     settings.APIKEY: "Required API key",
     settings.PUBLISHED_AFTER: "Fetch job ads published after specified date and time "
-                              "(format YYYY-mm-ddTHH:MM:SS)",
+    "(format YYYY-mm-ddTHH:MM:SS)",
     settings.PUBLISHED_BEFORE: "Fetch job ads published before specified date and time "
-                               "(format YYYY-mm-ddTHH:MM:SS)",
+    "(format YYYY-mm-ddTHH:MM:SS)",
     settings.FREETEXT_QUERY: "Freetext query. Search in ad headline, ad description and "
-                               "place of work",
+    "place of work",
     settings.FREETEXT_FIELDS: "Fields to freetext search in, in addition to default "
-                                "freetext search "
-                                "(parameter " + settings.FREETEXT_QUERY + ")\n"
-                              "Valid input values: " + str(QF_CHOICES) + "\n"
-                              "Default (no input): Search in ad headline, ad description "
-                                "and place of work",
+    "freetext search "
+    "(parameter " + settings.FREETEXT_QUERY + ")\n"
+    "Valid input values: " + str(QF_CHOICES) + "\n"
+    "Default (no input): Search in ad headline, ad description "
+    "and place of work",
     settings.EMPLOYER: "Name or organisation number (numbers only, no dashes or spaces) "
-                         "of employer",
+    "of employer",
     taxonomy.OCCUPATION: "One or more occupational codes according to the taxonomy",
     taxonomy.GROUP: "One or more occupational group codes according to the taxonomy",
     taxonomy.FIELD: "One or more occupational area codes according to the taxonomy",
     taxonomy.SKILL: "One or more competency codes according to the taxonomy",
     taxonomy.LANGUAGE: "One or more language codes according to the taxonomy",
-    taxonomy.DRIVING_LICENCE_REQUIRED: "Set to true if driving license required, false if not",
+    taxonomy.DRIVING_LICENCE_REQUIRED: "Set to true if driving license required"
+    ", false if not",
     taxonomy.DRIVING_LICENCE: "One or more types of demanded driving licenses, code "
-                                "according to the taxonomy",
+    "according to the taxonomy",
     taxonomy.EMPLOYMENT_TYPE: "Employment type, code according to the taxonomy",
     settings.EXPERIENCE_REQUIRED: "Input 'false' to filter jobs that don't require "
-                                    "experience",
+    "experience",
     taxonomy.WORKTIME_EXTENT: "One or more codes for worktime extent, code according to "
-                                "the taxonomy",
+    "the taxonomy",
     settings.PARTTIME_MIN: "For part-time jobs, minimum extent in percent "
-                             "(for example 50 for 50%)",
+    "(for example 50 for 50%)",
     settings.PARTTIME_MAX: "For part-time jobs, maximum extent in percent "
-                             "(for example 100 for 100%)",
+    "(for example 100 for 100%)",
     taxonomy.MUNICIPALITY: "One or more municipality codes, code according to "
-                             "the taxonomy",
+    "the taxonomy",
     taxonomy.REGION: "One or more region codes, code according to the taxonomy",
     taxonomy.COUNTRY: "One or more country codes, code according to the taxonomy",
     settings.POSITION: "Latitude and longitude in the format \"59.329,18.068\" "
-                         "(latitude,longitude)",
+    "(latitude,longitude)",
     settings.POSITION_RADIUS: "Radius from the specified " + settings.POSITION +
-                                " (latitude,longitude) in kilometers (km)",
+    " (latitude,longitude) in kilometers (km)",
 }
 swagger_filter_doc_params = {
     settings.DETAILS: "Show 'full' (default) or 'brief' results details",
     settings.OFFSET: "The offset parameter defines the offset from the first result you "
-                       "want to fetch"
-                     "Valid range is (0-%d)" % settings.MAX_OFFSET,
+    "want to fetch"
+    "Valid range is (0-%d)" % settings.MAX_OFFSET,
     settings.LIMIT: "Number of results to fetch (0-%d)" % settings.MAX_LIMIT,
     settings.SORT: "Sorting.\n"
-                   "relevance: relevance (points) (default sorting)\n"
-                   "pubdate-desc: published date, descending (newest job ad first)\n"
-                   "pubdate-asc: published date, ascending (oldest job ad first)\n"
-                   "applydate-desc: last apply date, descending (newest apply date first)\n"
-                   "applydate-asc: last apply date, descending (oldest apply date first, "
-                     "few days left for application)\n"
-                   "updated: sort by update date (descending)\n",
+    "relevance: relevance (points) (default sorting)\n"
+    "pubdate-desc: published date, descending (newest job ad first)\n"
+    "pubdate-asc: published date, ascending (oldest job ad first)\n"
+    "applydate-desc: last apply date, descending (newest apply date first)\n"
+    "applydate-asc: last apply date, descending (oldest apply date first, "
+    "few days left for application)\n"
+    "updated: sort by update date (descending)\n"
+    "id: sort by job ad id (ascending)\n",
     settings.STATISTICS: "Show statistics for specified fields "
-                           "(available fields: %s, %s and %s)" % (
+    "(available fields: %s, %s and %s)" % (
         taxonomy.OCCUPATION,
         taxonomy.GROUP,
         taxonomy.FIELD),

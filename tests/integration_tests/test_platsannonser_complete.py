21ae193474542a7c75e5b87c8fc3e1a4d15d760b
penms <mattias.persson@arbetsformedlingen.se>
2019-11-07 13:54:58 +0100

    Fixes autocomplete for 'c#'.

--- tests/integration_tests/test_platsannonser_complete.py
+++ tests/integration_tests/test_platsannonser_complete.py
@@ -37,7 +37,7 @@ def test_complete_one_param_occupation(synonym, expected):
             assert exp in complete_values
 
 
-@pytest.mark.skip(reason="Temporarily disabled, needs values in field keywords.enriched_synonyms")
+#@pytest.mark.skip(reason="Temporarily disabled, needs values in field keywords.enriched_synonyms")
 @pytest.mark.integration
 def test_complete_one_param_competence():
     print('==================', sys._getframe().f_code.co_name, '================== ')
@@ -60,7 +60,8 @@ def test_complete_one_param_competence():
         assert 'angularjs' in complete_values
 
 
-@pytest.mark.skip(reason="Temporarily disabled, needs values in field keywords.enriched_synonyms")
+
+#@pytest.mark.skip(reason="Temporarily disabled, needs values in field keywords.enriched_synonyms")
 @pytest.mark.integration
 def test_complete_two_params():
     print('==================', sys._getframe().f_code.co_name, '================== ')
@@ -81,6 +82,28 @@ def test_complete_two_params():
         # pprint(complete_values)
         assert 'angular' in complete_values
         assert 'angularjs' in complete_values
+
+
+# @pytest.mark.skip(reason="Temporarily disabled")
+@pytest.mark.integration
+def test_complete_one_param_competence_special_char():
+    print('==================', sys._getframe().f_code.co_name, '================== ')
+
+    app.testing = True
+    with app.test_client() as testclient:
+        headers = {'api-key': test_api_key, 'accept': 'application/json',
+                   settings.X_FEATURE_INCLUDE_SYNONYMS_TYPEAHEAD: 'true'}
+        result = testclient.get('/complete', headers=headers, data={'q': 'c#'})
+        json_response = result.json
+        # pprint(json_response)
+        assert 'typeahead' in json_response
+        json_typeahead = json_response['typeahead']
+
+        complete_values = [item['value'] for item in json_typeahead]
+
+        assert len(complete_values) > 0
+        # pprint(complete_values)
+        assert 'c#' in complete_values
 
 
 if __name__ == '__main__':

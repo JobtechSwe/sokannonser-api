da621b5db3926f2f9127aa2e061ec04150bc1cf6
Markus Sköld <markus.skold@arbetsformedlingen.se>
2020-02-26 13:03:36 +0100

    Sets prefix length to 0

--- sokannonser/repository/querybuilder.py
+++ sokannonser/repository/querybuilder.py
@@ -883,7 +883,8 @@ class QueryBuilder(object):
                    "skip_duplicates": True,
                    "fuzzy": {
                        "fuzziness": 'AUTO'
-                   }
+                   },
+                   "prefix_length": 0
                }
            )
         return search.to_dict()

8209288ca8882fd59a3029cfb1cc154236623c5e
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-04-02 13:02:06 +0200

    unskipped one test for logo moved test_api_key and headers variable to settings file removed commented out print()s

--- tests/integration_tests/test_fetch_platsannons_logo.py
+++ tests/integration_tests/test_fetch_platsannons_logo.py
@@ -3,16 +3,13 @@ import os
 import pytest
 from sokannonser import app
 from sokannonser.repository import platsannonser
+from tests.integration_tests.test_resources.settings import test_api_key, headers
 
-test_api_key = os.getenv('TEST_API_KEY')
 
-
-@pytest.mark.skip(reason="missing test data?")
 @pytest.mark.integration
 def test_fetch_org_logo_url_by_ad_id():
     app.testing = True
     with app.test_client() as testclient:
-        headers = {'api-key': test_api_key, 'accept': 'application/json'}
         # First do a search and use that ad:s ID to test fetch
         found_ads = testclient.get('/search', headers=headers, data={'limit': '100'})
         hits = found_ads.json.get("hits")
@@ -28,6 +25,30 @@ def test_fetch_org_logo_url_by_ad_id():
                 break
 
         assert found_logo_url
+
+
+@pytest.mark.integration
+def test_fetch_ad_logo_by_id():
+    print('==================', sys._getframe().f_code.co_name, '================== ')
+
+    app.testing = True
+    with app.test_client() as testclient:
+        # First do a search and use that ad:s ID to test fetch
+        found_ad = testclient.get('/search', headers=headers, data={'limit': '100'})
+        hits = found_ad.json.get("hits")
+        assert len(hits) > 0
+        found_logo_url_id = 0
+        for hit in hits:
+            ad_id = hit.get("id")
+            logo_url = platsannonser.get_correct_logo_url(ad_id)
+            if logo_url:
+                found_logo_url_id = ad_id
+                break
+
+        test_url = '/ad/' + str(found_logo_url_id) + '/logo'
+        result = testclient.get(test_url, headers=headers, data={})
+        assert result is not None
+        assert result.stream is not None
 
 
 @pytest.mark.skip(reason="missing test data?")
@@ -61,43 +82,6 @@ def test_fetch_wp_logo_url_only_org_logo_by_ad_id():
 def test_fetch_missing_logo_url_by_id():
     logo_url = platsannonser.get_correct_logo_url('10526669')
     assert logo_url is None
-
-
-@pytest.mark.skip(reason="missing test data?")
-@pytest.mark.integration
-def test_fetch_ad_logo_by_id():
-    print('==================', sys._getframe().f_code.co_name, '================== ')
-
-    app.testing = True
-    with app.test_client() as testclient:
-
-        headers = {'api-key': test_api_key, 'accept': 'application/json'}
-        # First do a search and use that ad:s ID to test fetch
-        found_ad = testclient.get('/search', headers=headers, data={'limit': '100'})
-        hits = found_ad.json.get("hits")
-        # print(hits)
-        assert len(hits) > 0
-
-        found_logo_url_id = 0
-        for hit in hits:
-            ad_id = hit.get("id")
-
-            logo_url = platsannonser.get_correct_logo_url(ad_id)
-            if logo_url:
-                found_logo_url_id = ad_id
-                # print(logo_url)
-                # print(found_logo_url_id)
-                break
-            # else:
-            #     print('Not found', ad_id)
-
-        test_url = '/ad/' + str(found_logo_url_id) + '/logo'
-        print(test_url)
-        result = testclient.get(test_url, headers=headers, data={})
-        # pprint(result.stream)
-
-        assert result is not None
-        assert result.stream is not None
 
 
 if __name__ == '__main__':

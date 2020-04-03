f646d150f7112067ec1630db91a6b1cc976cf6cc
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-04-03 08:21:10 +0200

    working on getting the skipped tests to run

--- tests/integration_tests/test_fetch_platsannons_logo.py
+++ tests/integration_tests/test_fetch_platsannons_logo.py
@@ -51,31 +51,6 @@ def test_fetch_ad_logo_by_id():
         assert result.stream is not None
 
 
-@pytest.mark.skip(reason="missing test data?")
-@pytest.mark.integration
-def test_fetch_ad_logo_by_id():
-    print('==================', sys._getframe().f_code.co_name, '================== ')
-
-    app.testing = True
-    with app.test_client() as testclient:
-        # First do a search and use that ad:s ID to test fetch
-        found_ad = testclient.get('/search', headers=headers, data={'limit': '100'})
-        hits = found_ad.json.get("hits")
-        assert len(hits) > 0
-        found_logo_url_id = 0
-        for hit in hits:
-            ad_id = hit.get("id")
-            logo_url = platsannonser.get_correct_logo_url(ad_id)
-            if logo_url:
-                found_logo_url_id = ad_id
-                break
-
-        test_url = '/ad/' + str(found_logo_url_id) + '/logo'
-        result = testclient.get(test_url, headers=headers, data={})
-        assert result is not None
-        assert result.stream is not None
-
-
 @pytest.mark.skip(reason="no test data with workplace id")
 @pytest.mark.integration
 def test_fetch_workplace_logo_url_by_ad_id():

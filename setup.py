e39bf3bf27e36394d5d9f80cace7971928254627
oleg l <oleg.lyalin@arbetsformedlingen.se>
2020-04-07 11:18:15 +0200

    change to flask_restx

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'jobtech-common', 'flask', 'flask-restplus', 'flask-cors', 'elasticsearch',
+        'jobtech-common', 'flask', 'flask_restx', 'flask-cors', 'elasticsearch',
         'certifi', 'requests'
     ],
     setup_requires=["pytest-runner"],

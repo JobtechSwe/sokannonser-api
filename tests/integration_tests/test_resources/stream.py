f0368fb6681000624e779f8cbee6a155c0531b8d
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-04-14 14:04:47 +0200

    jobstream  - adding test for occupation  - removed _ in name of function because it's in a separate file now

--- tests/integration_tests/test_resources/stream.py
+++ tests/integration_tests/test_resources/stream.py
@@ -1,7 +1,7 @@
 import json
 
 
-def _get_stream_check_number_of_results(session, url, expected_number, params):
+def get_stream_check_number_of_results(session, url, expected_number, params):
     response = session.get(f"{url}/stream", params=params)
     response.raise_for_status()
     assert response.content is not None

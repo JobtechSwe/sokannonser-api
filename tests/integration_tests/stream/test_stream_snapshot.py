09f8206cfa3c04f39aa366eb9d06ff79d7558546
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-04-16 16:30:06 +0200

    jobstream - adding test for snapshot - adding test for http 401 Unauthorized when the api key is wrong (stream, snapshot) - moved common code for response handling to separate function in stream.py

--- tests/integration_tests/stream/test_stream_snapshot.py
+++ tests/integration_tests/stream/test_stream_snapshot.py
@@ -1,16 +1,10 @@
 import pytest
 
-from tests.integration_tests.test_resources.stream import get_stream_check_number_of_results
+from tests.integration_tests.test_resources.stream import get_snapshot_check_number_of_results
 
-@pytest.mark.skip("to be defined how it should work")
-@pytest.mark.parametrize('date, expected_number', [
-    ('2000-01-01T00:00:01', 1065),  # all ads
-    ('2020-01-01T00:00:01', 1032),
-    ('2020-03-31T07:29:41', 74),
-    ('2020-04-25T07:29:41', 0)])
-def test_bulk(session, url, date, expected_number):
+@pytest.mark.smoke
+def test_snapshot(session, url):
     """
     Test snapshot with filtering on date
     """
-    params = {'date': date, 'snapshot': True}
-    get_stream_check_number_of_results(session, url, expected_number, params)
+    get_snapshot_check_number_of_results(session, url, expected_number=1065)
--- tests/integration_tests/test_resources/stream.py
+++ tests/integration_tests/test_resources/stream.py
@@ -1,16 +1,24 @@
 import json
-import requests
 
 
 def get_stream_check_number_of_results(session, url, expected_number, params):
     response = session.get(f"{url}/stream", params=params)
+    _check_ok_response_and_number_of_ads(response, expected_number)
+
+
+def get_stream_expect_error(session, url, path, params, expected_http_code):
+    r = session.get(f"{url}{path}", params=params)
+    status = r.status_code
+    assert status == expected_http_code, f"Expected http return code to be {expected_http_code} , but got {status}"
+
+
+def get_snapshot_check_number_of_results(session, url, expected_number):
+    response = session.get(f"{url}/snapshot")
+    _check_ok_response_and_number_of_ads(response, expected_number)
+
+
+def _check_ok_response_and_number_of_ads(response, expected_number):
     response.raise_for_status()
     assert response.content is not None
     list_of_ads = json.loads(response.content.decode('utf8'))
     assert len(list_of_ads) == expected_number, f'expected {expected_number} but got {len(list_of_ads)} ads'
-
-
-def get_stream_expect_error(session, url, params, expected_http_code):
-    r = session.get(f"{url}/stream", params=params)
-    status = r.status_code
-    assert status == expected_http_code, f"Expected http return code to be {expected_http_code} , but got {status}"

0e0de6b38f729d187440ed0327297249f9aa7750
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-04-16 16:06:09 +0200

    separated stream and search test files into  folders

--- tests/integration_tests/search/test_platsannonser_queries_enrichment.py
+++ tests/integration_tests/search/test_platsannonser_queries_enrichment.py
@@ -3,7 +3,7 @@ import sys
 import pytest
 
 from sokannonser import app
-from tests.integration_tests.test_platsannonser_queries import headers
+from tests.integration_tests.test_resources.settings import headers
 from tests.integration_tests.test_resources.check_response import check_response_return_json
 
 

bf8638d2fc4a30c555fadd0d122b1661b4eaf45b
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-04-16 15:24:45 +0200

    Jobstream - skipped test for stream snapshot until we have defined expected behavior

--- tests/integration_tests/stream/test_stream_bulk.py
+++ tests/integration_tests/stream/test_stream_bulk.py
@@ -2,23 +2,15 @@ import pytest
 
 from tests.integration_tests.test_resources.stream import get_stream_check_number_of_results
 
-
-
-
-
-
+@pytest.mark.skip("to be defined how it should work")
 @pytest.mark.parametrize('date, expected_number', [
-    ('2000-01-01T00:00:01', 1065),
+    ('2000-01-01T00:00:01', 1065),  # all ads
     ('2020-01-01T00:00:01', 1032),
-    ('2020-02-01T00:00:01', 971),
-    ('2020-02-25T07:29:41', 872),
-    ('2020-03-14T07:29:41', 556),
-    ('2020-03-25T07:29:41', 273),
-    ('2020-03-27T07:29:41', 186),
     ('2020-03-31T07:29:41', 74),
     ('2020-04-25T07:29:41', 0)])
 def test_bulk(session, url, date, expected_number):
     """
-    Test basic stream with filtering on date
+    Test snapshot with filtering on date
     """
-    get_stream_check_number_of_results(session, url, expected_number, params={'date': date})
+    params = {'date': date, 'snapshot': True}
+    get_stream_check_number_of_results(session, url, expected_number, params)

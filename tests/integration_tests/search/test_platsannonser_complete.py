30166c6a80eb8e1a8720a261cd02a3684e663e67
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-04-22 10:51:06 +0200

    improved test for 'x-feature-spellcheck-typeahead' with more test cases. Skipped until behavior is investigated (4 test cases failing)

--- tests/integration_tests/search/test_platsannonser_complete.py
+++ tests/integration_tests/search/test_platsannonser_complete.py
@@ -5,7 +5,7 @@ import pytest
 from sokannonser import app
 from sokannonser import settings
 from tests.integration_tests.test_resources.check_response import check_response_return_json
-from sokannonser.settings import headers
+from sokannonser.settings import test_api_key
 
 
 @pytest.mark.smoke
@@ -13,14 +13,14 @@ from sokannonser.settings import headers
 @pytest.mark.parametrize("query, synonyms, expect_success",
                          [('servit', ['servitris', 'servitör'], True),
                           ('servit', ['servitris', 'servitör', 'servitriser', 'servitörer'], True),
-                           ('systemutvecklare angu', ['angular', 'angularjs'], True),
-                           ('angu', ['angular', 'angularjs'], True),
-                           ('ang', ['angularjs', 'angular', 'angular.js', 'angered'], True),
-                           ('c#', ['c#'], True),
-                           ('pyth', ['python'], True),
-                           # check that synonyms are not suggested
-                           ('c#', ['c++'], False),
-                           ('c#', ['java'], False)])
+                          ('systemutvecklare angu', ['angular', 'angularjs'], True),
+                          ('angu', ['angular', 'angularjs'], True),
+                          ('ang', ['angularjs', 'angular', 'angular.js', 'angered'], True),
+                          ('c#', ['c#'], True),
+                          ('pyth', ['python'], True),
+                          # check that synonyms are not suggested
+                          ('c#', ['c++'], False),
+                          ('c#', ['java'], False)])
 def test_complete_endpoint_synonyms_typeahead(query, synonyms, expect_success):
     """
     Test that incomplete search queries will return synonyms
@@ -31,7 +31,9 @@ def test_complete_endpoint_synonyms_typeahead(query, synonyms, expect_success):
     """
     app.testing = True
     with app.test_client() as testclient:
-        headers[settings.X_FEATURE_INCLUDE_SYNONYMS_TYPEAHEAD] = 'true'
+        headers = {'api-key': test_api_key, 'accept': 'application/json',
+                   settings.X_FEATURE_INCLUDE_SYNONYMS_TYPEAHEAD: 'true'}
+
         result = testclient.get('/complete', headers=headers, data={'q': query})
         json_response = check_response_return_json(result)
         assert 'typeahead' in json_response
@@ -43,25 +45,46 @@ def test_complete_endpoint_synonyms_typeahead(query, synonyms, expect_success):
                 assert s in complete_values, f"Synonym '{s}' not found in response"
             else:
                 assert s not in complete_values, f"Synonym '{s}' was found in response"
-        print(synonyms)
 
-
-# This test case is for test complete endpoint with auto complete suggest
-# This test case is not good, will fix in future
+@pytest.mark.skip("4 failing test cases, skipped until investigated")
 @pytest.mark.integration
-def test_complete_endpoint_with_auto_complete_suggest():
+@pytest.mark.parametrize("query, expected_suggestions", [
+    ('servit', ['servicetekniker', 'servicearbete', 'service och underhåll', 'servicedesk', 'servicehandläggare',
+                'servicemedarbetare', 'serviceyrke', 'service manager', 'servicebiträde', 'serviceelektriker']),
+    ('systemutvecklare angu', ['systemutvecklare angularjs']),
+    ('angu', ['angularjs']),
+    ('c#', ['c#']),
+    ('c+', ['c++']),
+    ('ang', ['angularjs', 'angered']),
+    ('c', ['civilingenjör', 'c#', 'c', 'can', 'c körkort', 'cnc- operatör', 'c++', 'cad', 'cloud', 'css']),
+    ('pyth', ['python']),
+
+    # Failing
+    ('servit', ['servitris', 'servitör', 'servitriser', 'servitörer']),
+    ('servi', ['servicetekniker', 'servicearbete', 'service och underhåll', 'servicedesk', 'servicehandläggare',
+               'servicemedarbetare', 'serviceyrke', 'service manager', 'servicebiträde', 'serviceelektriker']),
+    ('angu', ['angular', 'angularjs']),
+    ('ang', ['angular', 'angularjs', 'angered'])
+])
+def test_complete_endpoint_with_auto_complete_suggest(query, expected_suggestions):
+    """
+    test of /complete endpoint with 'x-feature-spellcheck-typeahead' header
+    parameters: query and list of expected result(s)
+    """
     app.testing = True
     with app.test_client() as testclient:
-        query = 'pyth'
-        headers[settings.X_FEATURE_SPELLCHECK_TYPEAHEAD] = 'true'
+        headers = {'api-key': test_api_key, 'accept': 'application/json',
+                   settings.X_FEATURE_SPELLCHECK_TYPEAHEAD: 'true'}
         result = testclient.get('/complete', headers=headers, data={'q': query})
         json_response = check_response_return_json(result)
-
         assert 'typeahead' in json_response
+        actual_suggestions = [suggest.get('value') for suggest in json_response.get('typeahead')]
+        assert len(actual_suggestions) > 0, f"no suggested values as auto-complete for '{query}'"
+        assert len(actual_suggestions) == len(
+            expected_suggestions), f"\nQuery: {query}\nExpected suggestions: {expected_suggestions}\nActual suggestions: {actual_suggestions} "
+        for s in expected_suggestions:
+            assert s in actual_suggestions, f"\Did not find {query} in {actual_suggestions} "
 
-        suggest_value = [suggest.get('value') for suggest in json_response.get('typeahead')]
-        assert len(suggest_value) > 0, f"no suggested values as auto-complete for '{query}'"
-        assert 'python' in suggest_value, f"'python' not suggested as auto-complete for '{query}'"
 
 
 if __name__ == '__main__':

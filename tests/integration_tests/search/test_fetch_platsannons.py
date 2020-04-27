498b1a097afbf282b77f5118a724bbd95b4aa0f5
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-04-27 11:15:26 +0200

    removed unused import

--- tests/integration_tests/search/test_fetch_platsannons.py
+++ tests/integration_tests/search/test_fetch_platsannons.py
@@ -1,32 +1,33 @@
 import sys
 import os
+import http
 import pytest
 from sokannonser import app
-import http
-
-test_api_key = os.getenv('TEST_API_KEY')
-headers = {'api-key': test_api_key, 'accept': 'application/json'}
+from sokannonser.settings import headers
 
 
+@pytest.mark.smoke
 @pytest.mark.integration
 def test_fetch_ad_by_id():
+    """
+    Get an ad by a request to /search without a query,and limiting the result to one ad
+    use the id of the ad when doing a request to the /ad path
+    verify that the id of the ad is the same as used when doing the request
+    """
     print('==================', sys._getframe().f_code.co_name, '================== ')
     app.testing = True
     with app.test_client() as testclient:
         # First do a search and use that ad:s ID to test fetch
         found_ad = testclient.get('/search', headers=headers, data={'limit': '1'})
         assert found_ad.status_code == http.HTTPStatus.OK
-
         ad_id = found_ad.json.get("hits")[0].get("id")
+
         result = testclient.get('/ad/' + ad_id, headers=headers, data={})
         assert result.status_code == http.HTTPStatus.OK
         ad_result = result.json
 
         assert 'id' in ad_result
         assert ad_result['id'] == ad_id
-
-        # assert 'keywords' in ad_result
-        # assert 'enriched' not in ad_result['keywords']
 
 
 @pytest.mark.integration
@@ -37,8 +38,7 @@ def test_fetch_not_found_ad_by_id():
     with app.test_client() as testclient:
         ad_id = '823069282306928230692823069282306928230692'
         result = testclient.get('/ad/' + ad_id, headers=headers, data={})
-        assert result.status_code == 404
-        # ad_result = result.json
+        assert result.status_code == http.HTTPStatus.NOT_FOUND
 
 
 if __name__ == '__main__':

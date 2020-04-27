a26397a1c0ff571af945f0199d24981aa3838b78
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-04-27 10:24:28 +0200

    updated tests to handle new test data.

--- tests/integration_tests/stream/test_stream_snapshot.py
+++ tests/integration_tests/stream/test_stream_snapshot.py
@@ -1,10 +1,11 @@
 import pytest
 
 from tests.integration_tests.test_resources.stream import get_snapshot_check_number_of_results
+from sokannonser.settings import NUMBER_OF_ADS
 
 @pytest.mark.smoke
 def test_snapshot(session, url):
     """
     Test snapshot, should return everything
     """
-    get_snapshot_check_number_of_results(session, url, expected_number=1065)
+    get_snapshot_check_number_of_results(session, url, expected_number=NUMBER_OF_ADS)

37ad5bceb57519fe3d7c7d2ea4a0b3c6490251e4
baola <lina7906@gmail.com>
2020-04-28 21:24:27 +0200

    Add updated-before-date

--- bulkloader/rest/__init__.py
+++ bulkloader/rest/__init__.py
@@ -26,6 +26,8 @@ bulk_stream_query = reqparse.RequestParser()
 bulk_stream_query.add_argument(settings.APIKEY, location='headers', required=True)
 bulk_stream_query.add_argument(settings.DATE, type=inputs.datetime_from_iso8601,
                                required=True, default=default_time)
+bulk_stream_query.add_argument(settings.UPDATED_BEFORE_DATE, type=inputs.datetime_from_iso8601,
+                               required=False)
 
 bulk_snapshot_query = reqparse.RequestParser()
 bulk_snapshot_query.add_argument(settings.APIKEY, location='headers', required=True)

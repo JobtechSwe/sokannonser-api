6995a22d6c8d74a6f02805268e75a457437cadde
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-05-05 06:50:56 +0200

    Jobstream moved code for checking /zip endpoint to the actual test

--- tests/integration_tests/test_resources/stream.py
+++ tests/integration_tests/test_resources/stream.py
@@ -14,15 +14,6 @@ def get_stream_expect_error(session, url, path, params, expected_http_code):
     assert status == expected_http_code, f"Expected http return code to be {expected_http_code} , but got {status}"
 
 
-def get_zip_expect_connection_refused_error_or_not_found(session, url):
-    try:
-        r = session.get(f"{url}/zip")
-    except (IOError, urllib3.exceptions.HTTPError) as e:
-        pass  # not being able to connect to the endpoint is fine
-    else:
-        assert r.status_code == requests.codes.not_found
-
-
 def get_snapshot_check_number_of_results(session, url, expected_number):
     response = session.get(f"{url}/snapshot")
     _check_ok_response_and_number_of_ads(response, expected_number)

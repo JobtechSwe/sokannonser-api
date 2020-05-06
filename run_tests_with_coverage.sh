910e2e8599396956b48466c67ff6071dfbacdcf8
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-05-06 14:46:42 +0200

    Added shell script for running tests with coverage pytest-html for test report creation added to requirements.txt

---
+++ run_tests_with_coverage.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# This script will run tests with test coverage option
+# reports will be created in test_reports/
+#
+# args:
+# 1: api-key
+# 2: one of the three test types below
+# all = unit tests and integration tests
+# integration = integration tests
+# unit = unit tests
+
+if [ $# -ne 2 ]; then
+    echo "Error: Two arguments needed: api-key and test type [ all | unit | integration ]"
+    exit 1
+fi
+
+test_type=$2
+export TEST_API_KEY=$1
+
+if [ "$test_type" = "all" ]; then
+  test_to_run=''
+fi
+if [ "$test_type" = "unit" ]; then
+  test_to_run="unit_tests"
+fi
+
+if [ "$test_type" = "integration" ]; then
+  test_to_run="integration_tests"
+fi
+
+echo "starting "$test_type "tests"
+pytest --html=test_reports/$test_type/report/$test_type.html --cov=. --cov-config=tests/.coveragerc --cov-report html:test_reports/$test_type/coverage_$test_to_run tests/$test_to_run

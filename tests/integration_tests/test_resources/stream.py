e4d5479f53dfabc869fd1380563b4fa93e97cf6c
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-05-11 06:50:32 +0200

    Jobsearch
    modified helper function to handle search responses

--- tests/integration_tests/test_resources/stream.py
+++ tests/integration_tests/test_resources/stream.py
@@ -1,6 +1,5 @@
 import json
-import requests
-import urllib3
+
 
 
 def get_stream_check_number_of_results(session, url, expected_number, params):
@@ -16,13 +15,15 @@ def get_stream_expect_error(session, url, path, params, expected_http_code):
 
 def get_snapshot_check_number_of_results(session, url, expected_number):
     response = session.get(f"{url}/snapshot")
-    _check_ok_response_and_number_of_ads(response, expected_number)
+    return _check_ok_response_and_number_of_ads(response, expected_number)
 
 
 def _check_ok_response_and_number_of_ads(response, expected_number):
     response.raise_for_status()
     assert response.content is not None
     list_of_ads = json.loads(response.content.decode('utf8'))
+    if '/search' in response.url:
+        list_of_ads = list_of_ads['hits']
     assert len(list_of_ads) == expected_number, f'expected {expected_number} but got {len(list_of_ads)} ads'
 
     for ad in list_of_ads:
@@ -35,3 +36,4 @@ def _check_ok_response_and_number_of_ads(response, expected_number):
         checks.append(ad['workplace_address']['country'])
         for c in checks:
             assert c is not None, ad['id']
+    return response

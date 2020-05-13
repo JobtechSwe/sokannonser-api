c11810aedcd088dcb964e97d618db5631125d5a2
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-05-13 14:35:54 +0200

    Jobsearch modifed logo tests: removed Flask testclient, fixed bug in test, moved test into separate file

--- tests/integration_tests/search/test_fetch_platsannons_logo.py
+++ tests/integration_tests/search/test_fetch_platsannons_logo.py
@@ -2,12 +2,11 @@ import sys
 import os
 import pytest
 import requests
-from sokannonser import app
 from sokannonser.repository import platsannonser
-from sokannonser.settings import headers
+from tests.integration_tests.test_resources.helper import get_with_path_return_json
 
 
-def get_get_correct_logo_url_for_any_ad(list_of_ads):
+def get_correct_logo_url_for_any_ad(list_of_ads):
     assert len(list_of_ads) > 0
     for hit in list_of_ads:
         ad_id = hit.get("id")
@@ -16,76 +15,39 @@ def get_get_correct_logo_url_for_any_ad(list_of_ads):
         except requests.exceptions.HTTPError:
             pass
         else:
-            return True
-    return False
+            return logo_url
+    return False  # if no logo_url is found in list_of_ads
+
+
+@pytest.mark.live_data
+@pytest.mark.integration
+def test_fetch_org_logo_url_by_ad_id(session, search_url):
+    print('==================', sys._getframe().f_code.co_name, '================== ')
+    json_response = get_with_path_return_json(session, search_url, '/search', params={'limit': '100'})
+    hits = json_response['hits']
+    assert len(hits) > 0
+    assert get_correct_logo_url_for_any_ad(hits)
 
 
 @pytest.mark.integration
-def test_fetch_org_logo_url_by_ad_id():
+def test_fetch_ad_logo_by_id(session, search_url):
+    """
+    Search for 100 hits
+    Find the first logo url in those hits
+    GET that logo url
+    """
     print('==================', sys._getframe().f_code.co_name, '================== ')
 
-    app.testing = True
-    with app.test_client() as testclient:
-        # First do a search and use that ad:s ID to test fetch
-        found_ads = testclient.get('/search', headers=headers, data={'limit': '100'})
-        hits = found_ads.json.get("hits")
-        assert len(hits) > 0
+    json_response = get_with_path_return_json(session, search_url, '/search', params={'limit': '100'})
+    hits = json_response['hits']
+    assert len(hits) > 0
+    found_logo_url = get_correct_logo_url_for_any_ad(hits)
+    assert found_logo_url
 
-        assert get_get_correct_logo_url_for_any_ad(hits)
-
-
-@pytest.mark.integration
-def test_fetch_ad_logo_by_id():
-    print('==================', sys._getframe().f_code.co_name, '================== ')
-
-    app.testing = True
-    with app.test_client() as testclient:
-        # First do a search and use that ad:s ID to test fetch
-        found_ad = testclient.get('/search', headers=headers, data={'limit': '100'})
-        hits = found_ad.json.get("hits")
-        found_logo_url_id =  get_get_correct_logo_url_for_any_ad(hits)
-        assert found_logo_url_id
-        test_url = '/ad/' + str(found_logo_url_id) + '/logo'
-        result = testclient.get(test_url, headers=headers, data={})
-        assert result is not None
-        assert result.stream is not None
-
-
-@pytest.mark.skip(reason="no test data with workplace id")
-@pytest.mark.integration
-def test_fetch_workplace_logo_url_by_ad_id():
-    print('==================', sys._getframe().f_code.co_name, '================== ')
-
-    app.testing = True
-    with app.test_client():
-        ad_with_workplace_id = 23478773
-        ad_id = str(ad_with_workplace_id)
-        found_logo_url = platsannonser.get_correct_logo_url(ad_id)
-        print(found_logo_url)
-        assert found_logo_url
-
-
-@pytest.mark.skip(reason="no test data with workplace id")
-@pytest.mark.integration
-def test_fetch_wp_logo_url_only_org_logo_by_ad_id():
-    print('==================', sys._getframe().f_code.co_name, '================== ')
-
-    app.testing = True
-    with app.test_client():
-        ad_with_workplace_id = 8417304
-        ad_id = str(ad_with_workplace_id)
-        found_logo_url = platsannonser.get_correct_logo_url(ad_id)
-        print(found_logo_url)
-        assert found_logo_url
-
-
-@pytest.mark.skip(reason="no test data with workplace id")
-@pytest.mark.integration
-def test_fetch_missing_logo_url_by_id():
-    print('==================', sys._getframe().f_code.co_name, '================== ')
-
-    logo_url = platsannonser.get_correct_logo_url('10526669')
-    assert logo_url is None
+    result = requests.get(found_logo_url)
+    result.raise_for_status()
+    assert result is not None
+    assert len(result.content) > 1  # image
 
 
 if __name__ == '__main__':
---
+++ tests/integration_tests/search/test_logo_with_workplace_id.py
@@ -0,0 +1,43 @@
+import sys
+
+import pytest
+
+from sokannonser import app
+from sokannonser.repository import platsannonser
+
+
+@pytest.mark.skip(reason="no test data with workplace id")
+@pytest.mark.integration
+def test_fetch_workplace_logo_url_by_ad_id():
+    print('==================', sys._getframe().f_code.co_name, '================== ')
+
+    app.testing = True
+    with app.test_client():
+        ad_with_workplace_id = 23478773
+        ad_id = str(ad_with_workplace_id)
+        found_logo_url = platsannonser.get_correct_logo_url(ad_id)
+        print(found_logo_url)
+        assert found_logo_url
+
+
+@pytest.mark.skip(reason="no test data with workplace id")
+@pytest.mark.integration
+def test_fetch_wp_logo_url_only_org_logo_by_ad_id():
+    print('==================', sys._getframe().f_code.co_name, '================== ')
+
+    app.testing = True
+    with app.test_client():
+        ad_with_workplace_id = 8417304
+        ad_id = str(ad_with_workplace_id)
+        found_logo_url = platsannonser.get_correct_logo_url(ad_id)
+        print(found_logo_url)
+        assert found_logo_url
+
+
+@pytest.mark.skip(reason="no test data with workplace id")
+@pytest.mark.integration
+def test_fetch_missing_logo_url_by_id():
+    print('==================', sys._getframe().f_code.co_name, '================== ')
+
+    logo_url = platsannonser.get_correct_logo_url('10526669')
+    assert logo_url is None

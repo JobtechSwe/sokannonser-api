2e5da9e4191b7d9bab772672b6ba0b7944d1f53b
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-05-13 12:00:09 +0200

    Test changes: - added snapshot test for live data - marked test with 'live_data'

--- tests/integration_tests/search/test_live_data.py
+++ tests/integration_tests/search/test_live_data.py
@@ -1,6 +1,8 @@
+import json
 import pytest
 
-from tests.integration_tests.test_resources.helper import get_search
+from tests.integration_tests.test_resources.helper import get_search, get_stream, get_snapshot_check_number_of_results
+from sokannonser.settings import NUMBER_OF_ADS, UPDATED_BEFORE_DATE, DAWN_OF_TIME, current_time_stamp
 
 
 @pytest.mark.live_data
@@ -28,3 +30,21 @@ def test_search(session, search_url, query, minimum_number_of_hits):
     response = get_search(session, search_url, {'q': query, 'limit': '0'})
     assert response['total'][
                'value'] >= minimum_number_of_hits, f"Expected at least {minimum_number_of_hits} hits but got only {response['total']['value']} for query: {query}"
+
+
+@pytest.mark.live_data
+def test_stream_filter_on_date_interval(session, stream_url):
+    params = {'date': DAWN_OF_TIME, UPDATED_BEFORE_DATE: current_time_stamp}
+    list_of_ads = get_stream(session, stream_url, params)
+    assert len(list_of_ads) >= NUMBER_OF_ADS
+
+
+@pytest.mark.live_data
+def test_snapshot(session, stream_url):
+    """
+    Test snapshot, should return everything.
+    Check that number of ads is at least as many as in static test data (prod should have more)
+    """
+    response = get_snapshot_check_number_of_results(session, stream_url, expected_number=None)
+    list_of_ads = json.loads(response.content.decode('utf8'))
+    assert len(list_of_ads) >= NUMBER_OF_ADS

0a2b4e8fc7468e2843a1490bf7fdf8570a4a3f52
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-05-29 12:34:59 +0200

    jobsearch tests: deleted non-working tests. Since they were taxonomy-tests and that is deprecated, there is no point in spending more time on them

--- tests/api_tests/search/test_that_need_investigation_skipped.py
+++ tests/api_tests/search/test_that_need_investigation_skipped.py
@@ -57,42 +57,6 @@ def test_freetext_query_one_param_found_in_enriched_neg(session, search_url):
         assert hit['found_in_enriched'] is False, "field 'found_in_enriched' was not False as expected"
 
 
-@pytest.mark.integration
-@pytest.mark.skip("app.testclient.get return 0 hits. Without len check for hits, the test pretends to pass")
-def test_country_old(session, search_url):
-    print('==================', sys._getframe().f_code.co_name, '================== ')
-
-    _fetch_and_validate_result_old({taxonomy.REGION: '199'},
-                                   [fields.WORKPLACE_ADDRESS_REGION_CODE], ['199'])
-    _fetch_and_validate_result_old({taxonomy.REGION: '-199'},
-                                   [fields.WORKPLACE_ADDRESS_REGION_CODE], ['199'], False)
-
-
-@pytest.mark.skip("investigate why it doesn't work")
-@pytest.mark.skipif(not TEST_USE_STATIC_DATA, reason="depends on a fixed set of ads")
-@pytest.mark.parametrize("region, non_negative", [('199', True), ('-199', False)])
-def test_country(session, search_url, region, non_negative):
-    print('==================', sys._getframe().f_code.co_name, '================== ')
-
-    _fetch_and_validate_result(session, search_url, {taxonomy.REGION: region},
-                               [fields.WORKPLACE_ADDRESS_REGION_CODE], ['199'], non_negative)
-
-
-@pytest.mark.skip("investigate why it doesn't work")
-@pytest.mark.skipif(not TEST_USE_STATIC_DATA, reason="depends on a fixed set of ads")
-@pytest.mark.parametrize("query, path, expected",
-                         [({taxonomy.OCCUPATION: "D7Ns_RG6_hD2",
-                            taxonomy.MUNICIPALITY: "0180", "limit": 100},
-                           [fields.OCCUPATION + ".concept_id",
-                            fields.WORKPLACE_ADDRESS_MUNICIPALITY],
-                           ["D7Ns_RG6_hD2", "0180"]),
-                          ])
-@pytest.mark.integration
-def test_occupation_location_combo(session, search_url, query, path, expected):
-    print('==================', sys._getframe().f_code.co_name, '================== ')
-    _fetch_and_validate_result(session, search_url, query, path, expected)
-
-
 @pytest.mark.skip(
     reason="Temporarily disabled. Needs fix according to Trello Card #137, Multipla ord i ett yrke")  # Missing test data?
 @pytest.mark.integration

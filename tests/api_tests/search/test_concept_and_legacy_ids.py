5e1b9c979e7c4c20f3bedaaeef6344202439bae7
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-06-10 16:44:43 +0200

    Jobstream tests: added 'label' to tests and test data

--- tests/api_tests/search/test_concept_and_legacy_ids.py
+++ tests/api_tests/search/test_concept_and_legacy_ids.py
@@ -7,7 +7,11 @@ def test_all_ads_for_concept_and_legacy_ids(session, search_url):
         json_response = get_with_path_return_json(session, search_url, path=f"/ad/{test_data['id']}", params={})
         assert json_response['occupation']['concept_id'] == test_data['occupation_concept_id']
         assert json_response['occupation']['legacy_ams_taxonomy_id'] == test_data['occupation_legacy_id']
+        assert json_response['occupation']['label'] == test_data['occupation_label']
+
         assert json_response['occupation_group']['concept_id'] == test_data['occupation_group_concept_id']
         assert json_response['occupation_group']['legacy_ams_taxonomy_id'] == test_data['occupation_group_legacy_id']
+        assert json_response['occupation_group']['label'] == test_data['occupation_group_label']
+
         assert json_response['occupation_field']['concept_id'] == test_data['occupation_field_concept_id']
-        assert json_response['occupation_field']['legacy_ams_taxonomy_id'] == test_data['occupation_field_legacy_id']
+        assert json_response['occupation_field']['label'] == test_data['occupation_field_label']

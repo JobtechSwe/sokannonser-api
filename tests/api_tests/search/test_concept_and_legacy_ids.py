e92b34825eb0f61cfba211f2e38851f273f16e32
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-06-10 11:33:07 +0200

    Jobsearch tests: added test and test data for concept_ids and legacy_ams_taxonomy_id

---
+++ tests/api_tests/search/test_concept_and_legacy_ids.py
@@ -0,0 +1,15 @@
+import pytest
+from tests.test_resources.helper import get_with_path_return_json
+from tests.test_resources.concept_ids.ad_ids_with_all_concept_and_legacy_ids import all_ids
+
+
+@pytest.mark.slow
+def test_all_ads_for_concept_and_legacy_ids(session, search_url):
+    for test_data in all_ids:
+        json_response = get_with_path_return_json(session, search_url, path=f"/ad/{test_data['id']}", params={})
+        assert json_response['occupation']['concept_id'] == test_data['occupation_concept_id']
+        assert json_response['occupation']['legacy_ams_taxonomy_id'] == test_data['occupation_legacy_id']
+        assert json_response['occupation_group']['concept_id'] == test_data['occupation_group_concept_id']
+        assert json_response['occupation_group']['legacy_ams_taxonomy_id'] == test_data['occupation_group_legacy_id']
+        assert json_response['occupation_field']['concept_id'] == test_data['occupation_field_concept_id']
+        assert json_response['occupation_field']['legacy_ams_taxonomy_id'] == test_data['occupation_field_legacy_id']

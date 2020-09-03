02bc6498632c4d0f4d421d9ae478bc875b61a56a
baola <lina7906@gmail.com>
2020-09-03 14:53:52 +0200

    Change back freetext field problem

--- sokannonser/repository/querybuilder.py
+++ sokannonser/repository/querybuilder.py
@@ -455,7 +455,7 @@ class QueryBuilder(object):
         return ft_query
 
     def _freetext_fields(self, searchword, method=settings.DEFAULT_FREETEXT_BOOL_METHOD):
-        return {
+        return [{
             "multi_match": {
                 "query": searchword,
                 "type": "cross_fields",
@@ -464,17 +464,17 @@ class QueryBuilder(object):
                            f.DESCRIPTION_TEXT, f.ID, f.EXTERNAL_ID, f.SOURCE_TYPE,
                            f.KEYWORDS_EXTRACTED + ".location^5"]
             }
-        }
+        }]
 
     def _freetext_wildcard(self, searchword, wildcard_side, method=settings.DEFAULT_FREETEXT_BOOL_METHOD):
-        return {
+        return [{
             "multi_match": {
                 "query": searchword,
                 "type": "cross_fields",
                 "operator": method,
                 "fields": [f.HEADLINE + "." + wildcard_side, f.DESCRIPTION_TEXT + "." + wildcard_side]
             }
-        }
+        }]
 
 
     def _freetext_headline(self, query_dict, querystring):

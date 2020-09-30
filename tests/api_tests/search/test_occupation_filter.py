3475fdbca814fbe12a43d335350735a354e24c43
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-09-30 16:28:14 +0200

    Tests for occupation filter

--- tests/api_tests/search/test_occupation_filter.py
+++ tests/api_tests/search/test_occupation_filter.py
@@ -10,13 +10,28 @@ def test_occupation_filter(session, search_url, collection_info):
     using the concept ids in each collection.
     Does a query with 'occupation-filter' for each of the collection and checks that
     actual hits are as many as expected
-
-
     """
-    collection_id = collection_info['id']
-    expected = collection_info['expected']
-    label = collection_info['label']
-    print(label)
-    params = {'occupation-filter': collection_id, 'offset': 0, 'limit': 100}
+    params = {'occupation-filter': collection_info['id'], 'offset': 0, 'limit': 100}
     result_json = get_search(session, search_url, params)
-    assert result_json['total']['value'] == expected
+    assert result_json['total']['value'] == collection_info[
+        'expected'], f"collection id: {collection_info['id']}, label: {collection_info['label']}"
+
+
+@pytest.mark.parametrize("collection", occupation_collections.all_collections['data']['concepts'])
+def test_occupation_names_from_collection(session, search_url, collection):
+    list_of_concept_ids = list_of_concept_ids_from_concept(collection)
+    params = {'occupation-name': list_of_concept_ids, 'offset': 0, 'limit': 0}
+    result_json = get_search(session, search_url, params)
+    info = find_collection_info(collection['id'])
+    assert result_json['total']['value'] == info['expected']
+
+
+def list_of_concept_ids_from_concept(concept):
+    list_of_ids = []
+    for item in concept['related']:
+        list_of_ids.append(item['id'])
+    return list_of_ids
+
+
+def find_collection_info(concept_id):
+    return [x for x in occupation_collections.collections_info if x['id'] == concept_id][0]

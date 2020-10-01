82021ec988b1bbd21a790990a3fe1a7ca6518ca9
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-10-01 06:37:14 +0200

    Tests for occupation filter: changed to fetch collections when test is run and compare results with occuation-collection and occupation-name params

--- tests/api_tests/search/test_occupation_filter.py
+++ tests/api_tests/search/test_occupation_filter.py
@@ -1,29 +1,30 @@
 import pytest
 from tests.test_resources.helper import get_search
-from tests.test_resources.concept_ids import occupation_collections
+from sokannonser.repository.taxonomy import fetch_occupation_collections
+
 
 @pytest.mark.smoke
-@pytest.mark.parametrize("collection_info", occupation_collections.collections_info)
-def test_occupation_filter(session, search_url, collection_info):
+@pytest.mark.parametrize("collection", fetch_occupation_collections())
+def test_name_and_collection_param(session, search_url, collection):
     """
-    Takes info about how many hits are expected when searching with 'occupation-name'
-    using the concept ids in each collection.
-    Does a query with 'occupation-filter' for each of the collection and checks that
-    actual hits are as many as expected
+    Fetch collections from Taxonomy and test them one by one
+    Do a search with 'occupation-name' (using concept ids from collection) and save number of ads in result
+    Do a search with 'occupation-collection' and save number of ads in result
+    Verify that the number of ads are identical regardless of which param is used
     """
-    params = {'occupation-collection': collection_info['id'], 'offset': 0, 'limit': 100}
-    result_json = get_search(session, search_url, params)
-    assert result_json['total']['value'] == collection_info[
-        'expected'], f"collection id: {collection_info['id']}, label: {collection_info['label']}"
 
-
-@pytest.mark.parametrize("collection", occupation_collections.all_collections['data']['concepts'])
-def test_occupation_names_from_collection(session, search_url, collection):
+    # Search with 'occupation-name'
     list_of_concept_ids = list_of_concept_ids_from_concept(collection)
-    params = {'occupation-name': list_of_concept_ids, 'offset': 0, 'limit': 0}
-    result_json = get_search(session, search_url, params)
-    info = find_collection_info(collection['id'])
-    assert result_json['total']['value'] == info['expected']
+    params = {'occupation-name': list_of_concept_ids}
+    result_json_name = get_search(session, search_url, params)
+    number_of_ads_name = result_json_name['total']['value']
+
+    # search with 'occupation-collection'
+    params = {'occupation-collection': collection['id']}
+    result_json_collection = get_search(session, search_url, params)
+    number_of_ads_collection = result_json_collection['total']['value']
+
+    assert number_of_ads_collection == number_of_ads_name, collection['preferred_label']
 
 
 def list_of_concept_ids_from_concept(concept):
@@ -31,7 +32,3 @@ def list_of_concept_ids_from_concept(concept):
     for item in concept['related']:
         list_of_ids.append(item['id'])
     return list_of_ids
-
-
-def find_collection_info(concept_id):
-    return [x for x in occupation_collections.collections_info if x['id'] == concept_id][0]

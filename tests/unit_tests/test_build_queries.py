d92ec4d6b7c1359ce4c00e62770aaa7b8a0c8b99
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-10-06 10:14:51 +0200

    change unit test to use English. Move a queryBuilder unit test that mistakenly was marked as integration test

--- tests/unit_tests/test_build_queries.py
+++ tests/unit_tests/test_build_queries.py
@@ -3,23 +3,19 @@ import pytest
 
 
 @pytest.mark.unit
-@pytest.mark.parametrize("yrkessamlingar_id, expected", [(["UdVa_jRr_9DE"],
-                                                         {'bool': {'should': {'terms': {
-                                                             'occupation.concept_id.keyword': ['fFkk_8X8_pym',
-                                                                                               '4zLr_jP5_peZ',
-                                                                                               '5NxT_WeC_C31']}}}}),
-                                                         (["-UdVa_jRr_9DE"],
-                                                          {'bool': {'must_not': {'terms': {
-                                                              'occupation.concept_id.keyword': ['fFkk_8X8_pym',
-                                                                                                '4zLr_jP5_peZ',
-                                                                                                '5NxT_WeC_C31']}}}}),
-                                                         ([None],
-                                                          None),
-                                                         ([[]],
-                                                          None),
-                                                         (["None_existing_concept_id"],
-                                                          None)])
-def test_build_yrkessamlingar_query(yrkessamlingar_id, expected):
+@pytest.mark.parametrize("collection_id, expected", [(["UdVa_jRr_9DE"],
+                                                      {'bool': {'should': {'terms': {
+                                                          'occupation.concept_id.keyword': ['fFkk_8X8_pym',
+                                                                                            '4zLr_jP5_peZ',
+                                                                                            '5NxT_WeC_C31']}}}}),
+                                                     (["-UdVa_jRr_9DE"],
+                                                      {'bool': {'must_not': {'terms': {
+                                                          'occupation.concept_id.keyword': ['fFkk_8X8_pym',
+                                                                                            '4zLr_jP5_peZ',
+                                                                                            '5NxT_WeC_C31']}}}}),
+                                                     ([None], None), ([[]], None),
+                                                     (["None_existing_concept_id"], None)])
+def test_build_occupation_collection_query(collection_id, expected):
     querybuilder = QueryBuilder()
     querybuilder.occupation_collections = [
         {
@@ -37,5 +33,5 @@ def test_build_yrkessamlingar_query(yrkessamlingar_id, expected):
             ]
         }
     ]
-    queryresult = querybuilder.build_yrkessamlingar_query(yrkessamlingar_id)
-    assert queryresult == expected
+    query_result = querybuilder.build_yrkessamlingar_query(collection_id)
+    assert query_result == expected

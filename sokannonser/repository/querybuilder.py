271bed33aece8d9299ad457fcd81aa499b1fc68e
Carl Lernberg <carl.lernberg@arbetsformedlingen.se>
2020-10-16 11:32:33 +0200

    NAR-498 code review fix again

--- sokannonser/repository/querybuilder.py
+++ sokannonser/repository/querybuilder.py
@@ -412,7 +412,8 @@ class QueryBuilder(object):
         querystring = re.sub('\\s+', ' ', querystring).strip()
         return querystring
 
-    def _create_base_ft_query(self, querystring, method=settings.DEFAULT_FREETEXT_BOOL_METHOD):
+    def _create_base_ft_query(self, querystring, method):
+        method = 'and' if method == 'and' else settings.DEFAULT_FREETEXT_BOOL_METHOD
         # Creates a base query dict for "independent" freetext words
         # (e.g. words not found in text_to_concepts)
         suffix_words = ' '.join([w[1:] for w in querystring.split(' ')

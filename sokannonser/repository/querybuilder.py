24aa5888336e46fa95de5e65b37a1f904bae5caa
Carl Lernberg <carl.lernberg@arbetsformedlingen.se>
2020-10-16 11:07:18 +0200

    NAR-498 Removed unused code

--- sokannonser/repository/querybuilder.py
+++ sokannonser/repository/querybuilder.py
@@ -415,9 +415,6 @@ class QueryBuilder(object):
     def _create_base_ft_query(self, querystring, method=settings.DEFAULT_FREETEXT_BOOL_METHOD):
         # Creates a base query dict for "independent" freetext words
         # (e.g. words not found in text_to_concepts)
-        # Set method to 'or' as default (i.e. if not being set)
-        # method = 'and' if method == 'and' else 'or'
-
         suffix_words = ' '.join([w[1:] for w in querystring.split(' ')
                                  if w.startswith('*')])
         prefix_words = ' '.join([w[:-1] for w in querystring.split(' ')

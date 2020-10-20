87d9ba0db2f836f4779630e6a7347ec6466aa7bf
baola <lina7906@gmail.com>
2020-10-20 10:11:23 +0200

    Clean sokskrapade code

--- sokannonser/settings.py
+++ sokannonser/settings.py
@@ -121,5 +121,4 @@ URL_PROD = os.getenv('URL_PROD', 'https://jobsearch.api.jobtechdev.se/')
 # original index: narvalontology, new: jae-synonym-dictionary
 ONTOLOGY_INDEX = os.getenv('ONTOLOGY_INDEX', 'jae-synonym-dictionary')
 
-# scraped index
-ES_SCRAPED_INDEX = os.getenv("ES_SCRAPED_INDEX", "scrapedannons")
+

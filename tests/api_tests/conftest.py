ecaf421d7cd78246773aecb0cd3b4b5757bc2bd1
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-10-26 10:38:31 +0100

    removed tests for scraped ads

--- tests/api_tests/conftest.py
+++ tests/api_tests/conftest.py
@@ -27,17 +27,6 @@ def session_stream(scope="session"):
     s.headers.update(settings.headers_stream)
     return s
 
-@pytest.fixture
-def session_scraped(scope="session"):
-    """
-    creates a Session object which will persist over the entire test run ("session").
-    http connections will be reused (higher performance, less resource usage)
-    Returns a Session object
-    """
-    s = requests.sessions.Session()
-    s.headers.update(settings.headers_scraped)
-    return s
-
 
 @pytest.fixture
 def stream_url(scope="session"):
@@ -50,22 +39,10 @@ def stream_url(scope="session"):
 
 
 @pytest.fixture
-def scraped_url(scope="session"):
-    """
-    returns an url
-    """
-    test_url = os.getenv('TEST_URL_SCRAPED', 'http://127.0.0.1')
-    port = os.getenv('TEST_PORT_SCRAPED', 5000)
-    return f"{test_url}:{port}"
-
-@pytest.fixture
 def search_url(scope="session"):
     """
     returns an url
     """
     test_url = os.getenv('TEST_URL_SEARCH', 'http://localhost')
-    #test_url = 'https://dev-testset-jobsearch.test.services.jtech.se'
     port = os.getenv('TEST_PORT_SEARCH', 5000)
-    #port = 443
     return f"{test_url}:{port}"
-

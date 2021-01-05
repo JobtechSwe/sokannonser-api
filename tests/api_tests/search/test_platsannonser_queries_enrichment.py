eb84eb02e8434cf475572b529968dd7235e97aed
oskardrenske-ith <oskar.drenske@it-huset.se>
2021-01-05 09:39:42 +0100

    updated tests for new LA V2 test ads

--- tests/api_tests/search/test_platsannonser_queries_enrichment.py
+++ tests/api_tests/search/test_platsannonser_queries_enrichment.py
@@ -16,10 +16,16 @@ from tests.test_resources.helper import get_search, compare
     ('storlien', 0),
     ('fridhemsplan', 0)
 ])
-def test_freetext_query_location_extracted_or_enriched( session, geo, expected_number_of_hits):
+def test_freetext_query_location_extracted_or_enriched(session, geo, expected_number_of_hits):
     """
     Describe what the test is testing
     """
-    json_response = get_search(session,  params={'q': geo, 'limit': '0'})
+    json_response = get_search(session, params={'q': geo, 'limit': '0'})
     hits_total = json_response['total']['value']
     compare(hits_total, expected_number_of_hits)
+
+
+def test_freetext_query_synonym_param(session):
+    json_response = get_search(session, params={'q': 'montessori', 'limit': '100'})
+    assert json_response['freetext_concepts']['skill'][0] == 'montessoripedagogik'
+    compare(json_response['total']['value'], 2)
--- tests/api_tests/search/test_that_need_investigation_skipped.py
+++ tests/api_tests/search/test_that_need_investigation_skipped.py
@@ -1,30 +1,14 @@
-import sys
 import pytest
 from tests.test_resources.helper import get_search, compare
 
 
 @pytest.mark.skip(
-    reason="Temporarily disabled. Needs fix according to Trello Card #137, Multipla ord i ett yrke")  # Missing test data?
-@pytest.mark.integration
-def test_freetext_query_ssk( session):
-
+    reason="Temporarily disabled. Needs fix according to Trello Card #137, Multipla ord i ett yrke")
+def test_freetext_query_ssk(session):
     query = 'stockholm grundutbildad sjuksköterska'
-    json_response = get_search(session,  params={'q': query, 'limit': '100'})
+    json_response = get_search(session, params={'q': query, 'limit': '0'})
 
     assert json_response['freetext_concepts']['occupation'][0] == 'sjuksköterska'
     assert json_response['freetext_concepts']['location'][0] == 'stockholm'
     expected = 999999
     compare(json_response['total']['value'], expected=expected)
-
-
-@pytest.mark.skip("Test does not find expected ad")
-@pytest.mark.integration
-@pytest.mark.parametrize("synonym", ['montessori'])
-def test_freetext_query_synonym_param(session, synonym):
-    json_response = get_search(session,  params={'q': synonym, 'limit': '100'})
-    assert json_response['freetext_concepts']['skill'][0] == 'montessoripedagogik'
-
-    hits_total = json_response['total']['value']
-    compare(hits_total, 1)
-    # todo: Should get hits enriched with 'montessoripedagogik'.
-    #  ad 23891324 in testdata mentions 'montessoripedagogik' in description

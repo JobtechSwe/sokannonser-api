9d9b37e841079c0a6c97beaa5830a76b01f69a75
oskardrenske-ith <oskar.drenske@it-huset.se>
2021-01-05 15:05:54 +0100

    change regexp to only allow numbers in string format, added unit test for regexp

--- tests/integration_tests/search/test_taxonomy.py
+++ tests/integration_tests/search/test_taxonomy.py
@@ -1,15 +1,11 @@
-# -*- coding: utf-8 -*-
 import logging
 import pytest
-import os
 
 from sokannonser.repository import taxonomy as t
 from sokannonser.repository import platsannonser
-from sokannonser.repository.querybuilder import QueryBuilder
 import re
 
 log = logging.getLogger(__name__)
-pbquery = QueryBuilder()
 
 tax_stat = [[t.OCCUPATION], [t.GROUP], [t.FIELD], [t.SKILL]]
 tax_other = [[t.MUNICIPALITY], [t.REGION]]
@@ -19,7 +15,7 @@ tax_noexist = [['  ', 'blabla', '']]
 @pytest.mark.parametrize("taxonomy_type", tax_stat)
 def test_get_stats_for_taxonomy_type(taxonomy_type):
     for k, v in platsannonser.get_stats_for(taxonomy_type).items():
-        assert (re.match(r'^[\w\d_-]*$', k) is not None)  # check k is string of int
+        is_int(k)
         assert isinstance(v, int)  # check v is int
 
 
@@ -28,5 +24,22 @@ def test_get_stats_for_taxonomy_type_neg(taxonomy_type):
     assert platsannonser.get_stats_for(taxonomy_type) == {}
 
 
-if __name__ == '__main__':
-    pytest.main([os.path.realpath(__file__), '-svv', '-ra', '-m unit'])
+@pytest.mark.parametrize("v", ['a', 'abc', '-1'])
+def test_is_char_as_str(v):
+    with pytest.raises(AssertionError):
+        is_int(v)
+
+
+@pytest.mark.parametrize("v", ['1', '0', '10000'])
+def test_is_int_as_str(v):
+    is_int(v)
+
+
+@pytest.mark.parametrize("v", [0, 1, 1000])
+def test_is_int_as_int(v):
+    with pytest.raises(TypeError):
+        is_int(v)
+
+
+def is_int(value):
+    assert (re.match(r'[0-9]+$', value) is not None)
